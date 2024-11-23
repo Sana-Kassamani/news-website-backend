0a7884783e0d07772f0e66815e0586324d73e00b
Sana <Lenovo@DESKTOP-I4OODFD>
2024-11-24 01:25:55 +0200

    LAravel <> Add - editNews function and its route

--- app/Http/Controllers/Admin/NewsController.php
+++ app/Http/Controllers/Admin/NewsController.php
@@ -44,5 +44,39 @@ class NewsController extends Controller
             "news"=> $new_item
         ]);
     }
+    public function editNews(Request $request,$id){
+        $existing_news=NewsItem::find($id)->update([
+            "title"=> $request->title ,
+            "content"=> $request->content,
+            "minimum_age"=> $request->minimum_age,
+            "user_id"=> $request->user_id,
+        ]);
+        // if(!$existing_news)
+        // {
+        //     return response()->json([
+        //         "message"=> "News Item to edit doesn't exist"
+        //     ],404);
+
+        // }
+        // $item_param=[
+        //     "title"=> $request->title || ,
+        //     "content"=> $request->content,
+        //     "minimum_age"=> $request->minimum_age,
+        //     "user_id"=> $request->user_id,
+        // ];
+        // foreach($new_item_param as $key => $value)
+        // {
+        //     if (empty($value) && $value !== '0'){
+        //         return response()->json([
+        //             "message"=> "All fields are required"
+        //         ],400);
+        //     }
+
+        // }
+        // $new_item=NewsItem::create($new_item_param);
+        return response()->json([
+            "news"=> $existing_item
+        ]);
+    }
 
 }
--- routes/api.php
+++ routes/api.php
@@ -10,6 +10,7 @@ use App\Http\Controllers\Admin\NewsController;
 Route::prefix("/news")->group(function() {
   Route::get("/", [NewsController::class, "getNews"]);
   Route::post("/", [NewsController::class, "createNews"]);
+  Route::put("/{id}", [NewsController::class, "editNews"]);
 //   Route::get("/{id}", [CourseController::class, "get_course"]);
 //   Route::put("/{id}", [CourseController::class, "update_course"]);
 //   Route::delete("/{id}", [CourseController::class, "delete_course"]);
