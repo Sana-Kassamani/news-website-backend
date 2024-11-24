faa3dc601a8f9924617e6a1899e8913e353f2129
Sana <Lenovo@DESKTOP-I4OODFD>
2024-11-24 03:09:17 +0200

    Laravel <> Add - route for get users news

--- routes/api.php
+++ routes/api.php
@@ -3,16 +3,22 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\NewsController;
+use App\Http\Controllers\User\PostedNewsController;
 // use App\Http\Controllers\User\CourseController;
 
 
 
-Route::prefix("/news")->group(function() {
+Route::prefix("/admin")->group(function() {
   Route::get("/", [NewsController::class, "getNews"]);
   Route::post("/", [NewsController::class, "createNews"]);
   Route::put("/{id}", [NewsController::class, "editNews"]);
   Route::delete("/{id}", [NewsController::class, "deleteNews"]);
-//   Route::get("/{id}", [CourseController::class, "get_course"]);
-//   Route::put("/{id}", [CourseController::class, "update_course"]);
-//   Route::delete("/{id}", [CourseController::class, "delete_course"]);
 });
+
+
+Route::prefix("/user")->group(function() {
+    Route::get("/{id}", [PostedNewsController::class, "getUsersNews"]);
+    // Route::post("/", [NewsController::class, "createNews"]);
+    // Route::put("/{id}", [NewsController::class, "editNews"]);
+    // Route::delete("/{id}", [NewsController::class, "deleteNews"]);
+  });
